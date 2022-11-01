325ba81d7c4d38e425dcb6eca168b5f7aa0906d3
Paweł Kutny <kutny.pawel@gmail.com>
2022-11-01 21:15:56 +0100

    refactor: use opencv instead of pil

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-from PIL import Image
-
+import cv2 as cv
 
 CHARACTERS = ["@", "&", "$", "#", "%", "?", "+", ";", ":", ",", "."]
 
 
-def resize_image(image: Image, resized_image_width: int) -> Image:
-    original_width, original_height = image.size
-    resize_ratio = original_height / original_width / 2
+def resize_image(image: cv.Mat, resized_image_width: int) -> cv.Mat:
+    (original_height, original_width) = image.shape
+    resize_ratio = original_width / original_height / 2
     resized_image_height = int(resized_image_width * resize_ratio)
 
-    return image.resize((resized_image_width, resized_image_height))
+    return cv.resize(image, (resized_image_width, resized_image_height))
 
 
-def image_to_ascii_string(image: Image) -> str:
-    pixels = image.getdata()
+def image_to_ascii_string(image: cv.Mat) -> str:
+    pixels = image.flatten()
 
     return "".join([CHARACTERS[pixel//25] for pixel in pixels])
 
 
 def main(image_width=100) -> None:
-    with Image.open("./bad-apple.jpg") as image:
+    for i in range(1, 6573):
+        image = cv.imread(f"./frames/frame{i}.png", cv.IMREAD_GRAYSCALE)
+
         resized_image = resize_image(image, image_width)
-        grayscaled_image = resized_image.convert("L")
-        ascii_image_stringified = image_to_ascii_string(grayscaled_image)
+        ascii_image_stringified = image_to_ascii_string(resized_image)
         ascii_image = "\n".join([ascii_image_stringified[i:(i+image_width)]
                                 for i in range(0, len(ascii_image_stringified), image_width)])
 
