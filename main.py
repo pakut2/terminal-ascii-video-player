beff5c8c90ebec2f6742aeae4980075bd5362ac6
Paweł Kutny <kutny.pawel@gmail.com>
2022-11-02 21:31:22 +0100

    fix: use performance counter instead of time

--- main.py
+++ main.py
@@ -40,13 +40,13 @@ def play_video(horizontal_resolution=100) -> None:
     frame_duration = 1 / FPS
 
     for i in range(1, len(os.listdir("./frames"))):
-        frame_start = time.time()
+        frame_start = time.perf_counter()
 
         with Image.open(f"./frames/frame{i}.png") as frame:
             ascii_frame = convert_frame(frame, horizontal_resolution)
             print(ascii_frame)
 
-        frame_end = time.time()
+        frame_end = time.perf_counter()
         frame_elapsed = frame_end - frame_start
         frame_delay = frame_duration - frame_elapsed
 
