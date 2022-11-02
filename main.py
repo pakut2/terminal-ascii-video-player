ff29a3858559166b11e73f2f6b5a40ea7ead4ed8
Paweł Kutny <kutny.pawel@gmail.com>
2022-11-02 14:58:55 +0100

    fix: optimize

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 import cv2 as cv
+import os
+import time
+import sys
+
 
 CHARACTERS = ["@", "&", "$", "#", "%", "?", "+", ";", ":", ",", "."]
+BRIGHTNESS_TO_ASCII_SCALE = 255 // (len(CHARACTERS) - 1)
+FPS = 24
 
 
 def resize_image(image: cv.Mat, resized_image_width: int) -> cv.Mat:
     (original_height, original_width) = image.shape
-    resize_ratio = original_width / original_height / 2
+    resize_ratio = original_height / original_width / 2
     resized_image_height = int(resized_image_width * resize_ratio)
 
     return cv.resize(image, (resized_image_width, resized_image_height))
 
 
 def image_to_ascii_string(image: cv.Mat) -> str:
-    pixels = image.flatten()
+    ascii_image: list[str] = []
 
-    return "".join([CHARACTERS[pixel//25] for pixel in pixels])
+    for row in image:
+        ascii_string = "".join(
+            [CHARACTERS[pixel//BRIGHTNESS_TO_ASCII_SCALE] for pixel in row])
+        ascii_string += "\n"
+        ascii_image.append(ascii_string)
+
+    return ascii_image
 
 
 def main(image_width=100) -> None:
-    for i in range(1, 6573):
+    frame_duration = 1 / FPS
+
+    for i in range(1, len(os.listdir("./frames")) - 1):
+        frame_start = time.time()
         image = cv.imread(f"./frames/frame{i}.png", cv.IMREAD_GRAYSCALE)
 
         resized_image = resize_image(image, image_width)
-        ascii_image_stringified = image_to_ascii_string(resized_image)
-        ascii_image = "\n".join([ascii_image_stringified[i:(i+image_width)]
-                                for i in range(0, len(ascii_image_stringified), image_width)])
+        ascii_image_array = image_to_ascii_string(resized_image)
+        ascii_image = "".join(ascii_image_array)
 
-        print(ascii_image)
+        sys.stdout.write(ascii_image)
+        sys.stdout.flush()
+
+        frame_end = time.time()
+        frame_delay = frame_duration - (frame_end - frame_start)
+        if (frame_delay > 0):
+            time.sleep(frame_delay)
 
 
-main()
+main(200)
