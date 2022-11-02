ec84c346798dafd4328457e90ca9de0091d4217a
Paweł Kutny <kutny.pawel@gmail.com>
2022-11-02 17:01:47 +0100

    chore: naming

--- main.py
+++ main.py
@@ -36,14 +36,14 @@ def print_image(image_width=300) -> None:
         print(ascii_image)
 
 
-def main(frame_width=100) -> None:
+def main(horizontal_resolution=100) -> None:
     frame_duration = 1 / FPS
 
     for i in range(1, len(os.listdir("./frames"))):
         frame_start = time.time()
 
         with Image.open(f"./frames/frame{i}.png") as frame:
-            ascii_frame = convert_frame(frame, frame_width)
+            ascii_frame = convert_frame(frame, horizontal_resolution)
             print(ascii_frame)
 
         frame_end = time.time()
