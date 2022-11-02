f63147c53324896b35671cefece014e05618b738
Paweł Kutny <kutny.pawel@gmail.com>
2022-11-02 13:59:47 +0100

    feat: print single image

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import time
 
 
 CHARACTERS = ["@", "&", "$", "#", "%", "?", "+", ";", ":", ",", "."]
-FPS = 30
+BRIGHTNESS_TO_ASCII_SCALE = 255 // (len(CHARACTERS) - 1)
+FPS = 24
 
 
 def resize_frame(frame: Image, resized_image_width: int) -> Image:
     original_width, original_height = frame.size
     resize_ratio = original_height / original_width / 2
     resized_image_height = int(resized_image_width * resize_ratio)
-    
+
     return frame.resize((resized_image_width, resized_image_height))
 
 
 def frame_to_ascii_string(frame: Image) -> str:
     pixels = frame.getdata()
-    return "".join([CHARACTERS[pixel//25] for pixel in pixels])
+
+    return "".join([CHARACTERS[pixel//BRIGHTNESS_TO_ASCII_SCALE] for pixel in pixels])
 
 
 def convert_frame(frame: Image, frame_width=100) -> str:
@@ -25,6 +27,12 @@ def convert_frame(frame: Image, frame_width=100) -> str:
     ascii_frame_stringified = frame_to_ascii_string(grayscaled_frame)
     return "\n".join([ascii_frame_stringified[i:(i+frame_width)]
                       for i in range(0, len(ascii_frame_stringified), frame_width)])
+
+
+def print_image(image_width=300) -> None:
+    with Image.open("./shrek.jpeg") as image:
+        ascii_image = convert_frame(image, image_width)
+        print(ascii_image)
 
 
 def main(frame_width=100) -> None:
@@ -40,7 +48,8 @@ def main(frame_width=100) -> None:
 
         frame_end = time.time()
         frame_delay = frame_duration - (frame_end - frame_start)
-        time.sleep(frame_delay)
+        if (frame_delay > 0):
+            time.sleep(frame_delay)
 
 
 main()
